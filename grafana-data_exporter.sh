87683aeae4a8e65432aaf9d29bf06ae1c05f5c49
Kevin GILLET <KGILLET@groupement.systeme-u.fr>
2020-10-21 11:04:45 +0200

    comment change

--- grafana-data_exporter.sh
+++ grafana-data_exporter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Export the db returned values to csv format
 curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=devconnected" --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'" -H "Accept: application/csv" > raw-csv-data.csv
-#Call python script to write csv data to google spreadsheet
+#Make alias to correctly call python from bash
 alias python='winpty python.exe'
+#Call python script to write csv data to google spreadsheet
 python send-csv_google-sheets.py
