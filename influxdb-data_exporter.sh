487320b65df0b8282384ee944906d2a920e41732
Kevin GILLET <KGILLET@groupement.systeme-u.fr>
2020-10-23 09:06:22 +0200

    Comments clarification

--- influxdb-data_exporter.sh
+++ influxdb-data_exporter.sh
@@ -5,7 +5,8 @@
 #curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
 #Export the db returned values to csv format
 curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=mydb" --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'" -H "Accept: application/csv" > raw-csv-data.csv
-#Make alias to correctly call python from bash - ONLY FOR WINDOWS !
+#Make alias to correctly call python from bash
+#Uncomment the next line ONLY FOR WINDOWS !
 alias python='winpty python.exe'
 #Removing the first csv line
 sed 1d raw-csv-data.csv > formatted-csv-data.csv
