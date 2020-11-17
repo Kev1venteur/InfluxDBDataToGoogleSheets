47fb78ebde68ffae2933eb97c13e667b7f034353
Kev1venteur <kvin.gillet@gmail.com>
2020-11-17 10:19:24 +0100

    Reorder des fichiers

--- launcher.sh
+++ launcher.sh
@@ -7,6 +7,9 @@ sed 1d raw-influx-csv-data.csv > formatted-influx-csv-data.csv
 #Call bash script to export Oracle data to CSV
 echo "Getting data from Oracle..."
 ./oracle-data_export/oracle-data_exporter.sh
+#Make alias to correctly call python from bash
+#Uncomment the next line ONLY FOR WINDOWS !
+alias python='winpty python.exe'
 #Call python script to write csv data to google spreadsheet
 echo "Sending data to Google Sheets..."
 python send-csv_google-sheets.py
