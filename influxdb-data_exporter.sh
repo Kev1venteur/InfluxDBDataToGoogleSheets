7507e8e6a0e570b2b76d17ba96796e0237f40b28
Kev1venteur <kvin.gillet@gmail.com>
2020-11-17 13:29:16 +0100

    Better code readability

--- influxdb-data_exporter.sh
+++ influxdb-data_exporter.sh
@@ -5,3 +5,4 @@
 #curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
 #Export the db returned values to csv format
 curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=mydb" --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'" -H "Accept: application/csv" > csv/raw/raw-influx-data.csv
+echo
--- launcher.sh
+++ launcher.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-#-----------------------------CAUTION---------------------------------#
+#---------------------------------CAUTION--------------------------------------#
 #If the script does not works for any dark reasons, try deleting your
 #token.picle file in credentials folder & restart your internet
 #connection.
-#---------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 #Call bash script to export InfluxDB data to CSV
 echo "Getting data from InfluxDB..."
 echo
 ./influxdb-data_exporter.sh
 #Removing the first csv line
 sed 1d csv/raw/raw-influx-data.csv > csv/formatted/formatted-influx-data.csv
+echo "InfluxDB data correctly formatted to CSV normalisation."
+echo
 #Call bash script to export Oracle data to CSV
 echo "Getting data from Oracle..."
 echo
 ./oracle-data_export/oracle-data_exporter.sh
 #Make alias to correctly call python from bash
-#----------------------------------------WINDOWS---------------------------------------------------#
+#---------------------------------WINDOWS--------------------------------------#
 #Uncomment the next line ONLY FOR WINDOWS !
 alias python='winpty python.exe'
-#-----------------------------------------PROXY----------------------------------------------------#
+#----------------------------------PROXY---------------------------------------#
 #If you have a proxy uncomment the next line and put your proxy cert path
 export REQUESTS_CA_BUNDLE=credentials/cacert.pem
-#Then duplicate your cert to the path returned by "locate-cert-path.py" with the name "cacert.pem"
+#Then duplicate your cert to the path returned by "locate-cert-path.py"
+#with the name "cacert.pem"
 #You can execute it by typing : "python *scriptfolderpath*/locate-cert-path.py"
-#--------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 #Call python script to write csv data to google spreadsheet
-echo "Sending data to Google Sheets..."
+echo "Sending all data to Google Sheets..."
 python send-csv_google-sheets.py
-#Pause to see the log
+#Pause to see the terminal log
 echo
-read -p "Press enter to continue"
+read -p "End, press enter to continue"
