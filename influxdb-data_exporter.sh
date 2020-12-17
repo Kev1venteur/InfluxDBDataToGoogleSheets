db32c39b19ec537761a930997d27fbf78febd13a
Kev1venteur <kvin.gillet@gmail.com>
2020-12-17 16:24:38 +0100

    modifying path to keep only pertinents names into formatted folder

--- influxdb-data_exporter.sh
+++ influxdb-data_exporter.sh
@@ -6,13 +6,13 @@
 #curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 'cpu_load_short,host=u3recu197.recgroupement.systeme-u.fr,region=us-west value=0.64 1434055562000000000'
 #----------------------------Data Processing-------------------------------------------#
 #Foreach hostname, getting data from influxdb directly into separated CSV format
-cat csv/formatted/formatted-temboard-data.csv | while read hostname
+cat csv/temboard-hostnames.csv | while read hostname
 do
   curl -s -S -G 'http://localhost:8086/query?pretty=true'\
           --data-urlencode "db=mydb"\
           --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"host\"='"$hostname"'"\
           -H "Accept: application/csv" > 'csv/raw/raw-influx('"$hostname"')-data.csv'
   #Removing the first csv's line
-  sed 1d 'csv/raw/raw-influx('"$hostname"')-data.csv' > 'csv/formatted/formatted-influx('"$hostname"')-data.csv'
+  sed 1d 'csv/raw/raw-influx('"$hostname"')-data.csv' > 'csv/formatted/'"$hostname"'.csv'
 done
 echo "InfluxDB data correctly formatted to CSV normalisation."
