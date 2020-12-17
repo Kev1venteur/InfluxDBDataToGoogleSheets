522d3785e66985c489a4ffe2214cf835b9676731
Kev1venteur <kvin.gillet@gmail.com>
2020-12-17 16:02:22 +0100

    Getting Influx data foreach hostname from temboard

--- influxdb-data_exporter.sh
+++ influxdb-data_exporter.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+#----------------------------DB Managment for tests------------------------------------#
 #Create DB via http
 #curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE mydb"
-#White data into db
-#curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
-#Export the db returned values to csv format
-curl -s -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=mydb" --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'" -H "Accept: application/csv" > csv/raw/raw-influx-data.csv
-#Removing the first csv line
-sed 1d csv/raw/raw-influx-data.csv > csv/formatted/formatted-influx-data.csv
+#Write data into db
+#curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 'cpu_load_short,host=u3recu197.recgroupement.systeme-u.fr,region=us-west value=0.64 1434055562000000000'
+#----------------------------Data Processing-------------------------------------------#
+#Foreach hostname, getting data from influxdb directly into separated CSV format
+cat csv/formatted/formatted-temboard-data.csv | while read hostname
+do
+  curl -s -G 'http://localhost:8086/query?pretty=true'\
+          --data-urlencode "db=mydb"\
+          --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"host\"='"$hostname"'"\
+          -H "Accept: application/csv" > 'csv/raw/raw-influx('"$hostname"')-data.csv'
+  #Removing the first csv's line
+  sed 1d 'csv/raw/raw-influx('"$hostname"')-data.csv' > 'csv/formatted/formatted-influx('"$hostname"')-data.csv'
+done
 echo "InfluxDB data correctly formatted to CSV normalisation."
