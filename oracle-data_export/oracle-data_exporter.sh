f275a5ce9d97ae3594e5517f7e9acfd60852dc92
Kev1venteur <kvin.gillet@gmail.com>
2021-01-19 11:30:45 +0100

    Ram cpu et disk ok pour oracle

--- oracle-data_export/oracle-data_exporter.sh
+++ oracle-data_export/oracle-data_exporter.sh
@@ -3,8 +3,10 @@
 # Load database connection info
 source oracle-data_export/.env
 
-# Read query into a variable
-sql="$(cat oracle-data_export/oracle-query.sql)"
+# Read queries into variables
+sqlram="$(cat oracle-data_export/oracle-query-ram.sql)"
+sqlcpu="$(cat oracle-data_export/oracle-query-cpu.sql)"
+sqldisk="$(cat oracle-data_export/oracle-query-disk.sql)"
 
 # If sqlplus is not installed, then exit
 if ! command -v oracle-data_export/instantclient_19_6/sqlplus.exe > /dev/null; then
@@ -13,13 +15,17 @@ if ! command -v oracle-data_export/instantclient_19_6/sqlplus.exe > /dev/null; t
 fi
 
 # Connect to the database, run the query, then disconnect
-echo -e "SET PAGESIZE 0\n SET FEEDBACK OFF\n $sql" | \
-oracle-data_export/instantclient_19_6/sqlplus.exe -S -L "$ORACLE_USERNAME/$ORACLE_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ORACLE_HOST)(PORT=$ORACLE_PORT))(CONNECT_DATA=(SERVICE_NAME=$ORACLE_DATABASE)))" > csv/raw/raw-oracle-data.csv
+#Sed replaces spaces by commas and add etiquettes
 
-#Put commas instead of spaces in CSV
-sed -e 's/\s\+/,/g' csv/raw/raw-oracle-data.csv > csv/raw/temp-oracle-data
-#Add "Etiquette" to each lines
-sed 's/^/,Moy_Ram/' csv/raw/temp-oracle-data > csv/raw/raw-oracle-data.csv
+echo -e "SET PAGESIZE 0\n SET FEEDBACK OFF\n $sqlram" | \
+oracle-data_export/instantclient_19_6/sqlplus.exe -S -L "$ORACLE_USERNAME/$ORACLE_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ORACLE_HOST)(PORT=$ORACLE_PORT))(CONNECT_DATA=(SERVICE_NAME=$ORACLE_DATABASE)))" | sed -e 's/\s\+/,/g' | sed 's/^/,Moy_RAM/'> csv/raw/raw-oracle-data.csv
+
+echo -e "SET PAGESIZE 0\n SET FEEDBACK OFF\n $sqlcpu" | \
+oracle-data_export/instantclient_19_6/sqlplus.exe -S -L "$ORACLE_USERNAME/$ORACLE_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ORACLE_HOST)(PORT=$ORACLE_PORT))(CONNECT_DATA=(SERVICE_NAME=$ORACLE_DATABASE)))" | sed -e 's/\s\+/,/g' | sed 's/^/,Moy_CPU/' >> csv/raw/raw-oracle-data.csv
+
+echo -e "SET PAGESIZE 0\n SET FEEDBACK OFF\n $sqldisk" | \
+oracle-data_export/instantclient_19_6/sqlplus.exe -S -L "$ORACLE_USERNAME/$ORACLE_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ORACLE_HOST)(PORT=$ORACLE_PORT))(CONNECT_DATA=(SERVICE_NAME=$ORACLE_DATABASE)))" | sed -e 's/\s\+/,/g' | sed 's/^/,Moy_Disk/' >> csv/raw/raw-oracle-data.csv
+
 #Add "Cible" to each lines
 sed 's/^/,u3recu111/' csv/raw/raw-oracle-data.csv > csv/raw/temp-oracle-data
 #Add dates to CSV
