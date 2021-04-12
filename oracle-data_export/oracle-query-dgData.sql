f76856fc4742774f6f590be4154844a35e384330
Kev1venteur <kvin.gillet@gmail.com>
2021-04-12 15:25:19 +0200

    Max value of RECO and avg value of DATA on 2days

--- oracle-data_export/oracle-query-dgData.sql
+++ oracle-data_export/oracle-query-dgData.sql
@@ -1,9 +1,11 @@
 -- This file must end in a new line.
 --DiskGroup Usage of DATA diskgroup
+--Moyenne de valeur sur les 2 derniers jours
 select round(avg(avg_value))
 from sysman.gc${dollar}metric_values_daily
 where metric_group_name = 'DiskGroup_Usage'
 and metric_column_label = 'Disk Group Used %'
 and lower(SUBSTR(entity_name,6)) like '%${envhostname}%'
-and upper(key_part_1) = 'DATA';
+and upper(key_part_1) = 'DATA'
+and collection_time between SYSDATE - 2 and SYSDATE;
 exit
--- oracle-data_export/oracle-query-dgReco.sql
+++ oracle-data_export/oracle-query-dgReco.sql
@@ -1,9 +1,11 @@
 -- This file must end in a new line.
 --DiskGroup Usage of DATA diskgroup
-select round(avg(avg_value))
+--valeur la plus élevée
+select MAX(MAX_VALUE)
 from sysman.gc${dollar}metric_values_daily
 where metric_group_name = 'DiskGroup_Usage'
 and metric_column_label = 'Disk Group Used %'
 and lower(SUBSTR(entity_name,6)) like '%${envhostname}%'
-and upper(key_part_1) = 'RECO';
+and upper(key_part_1) = 'RECO'
+and collection_time between ADD_MONTHS((LAST_DAY(add_months(sysdate,-1))+1),-1) and ADD_MONTHS((LAST_DAY(sysdate)+1),-1);
 exit
