592ec30b0b96dd9861ec364861c16ee06909c150
Kev1venteur <kvin.gillet@gmail.com>
2021-04-14 12:33:51 +0200

    Ajout requete SQL pour taux de dispo d'un host dans le mois en cours

--- oracle-data_export/oracle-query-availability.sql
+++ oracle-data_export/oracle-query-availability.sql
@@ -1,25 +1,9 @@
-SELECT
-     A.target_name DBNAME,
-     LOWER(A.AVAILABILITY_STATUS) AVAI_STATUS,
-     SUM(ROUND(least(nvl( MGMT_VIEW_UTIL.ADJUST_TZ(A.end_timestamp,B.TIMEZONE_REGION,'Europe/Paris'),(MGMT_VIEW_UTIL.ADJUST_TZ(sysdate,sessiontimezone,'Europe/Paris'))),to_date(:datefin, 'yyyymmdd') )-greatest( MGMT_VIEW_UTIL.ADJUST_TZ(A.start_timestamp,B.TIMEZONE_REGION,'Europe/Paris'), to_date(:datedeb, 'yyyymmdd')),8))*24*60 DURATION
- FROM
-   mgmt$availability_history A,MGMT$TARGET B
- WHERE A.target_name like 'O%' and
- (A.target_type='rac_database') AND A.TARGET_GUID=B.TARGET_GUID
-   and MGMT_VIEW_UTIL.ADJUST_TZ(start_timestamp,B.TIMEZONE_REGION,'Europe/Paris') < to_date(:datefin, 'yyyymmdd') and
-   (MGMT_VIEW_UTIL.ADJUST_TZ(end_timestamp,B.TIMEZONE_REGION,'Europe/Paris')>= to_date(:datedeb, 'yyyymmdd') OR end_timestamp is NULL)
-GROUP BY A.target_name,A.AVAILABILITY_STATUS ORDER BY A.target_name;
-
-
--- 1 requete à l'année
--- 1 requ�te au mois
-
-
-SELECT empno, deptno,
-COUNT(*) OVER (PARTITION BY
-deptno) DEPT_COUNT
-FROM emp
-WHERE deptno IN (20, 30);
-
--- - de 60% de up sur la totalité => base non prise en compte
--- + indiquer les bases en écart
+-- This file must end in a new line.
+-- Total Percentage UP in the current month
+select round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 3)
+from sysman.MGMT$AVAILABILITY_HISTORY
+where target_type = 'host'
+and availability_status = 'Blackout'
+and target_name = '%${envhostname}%'
+and TO_CHAR(start_timestamp,'MM') = TO_CHAR(LAST_DAY(SYSDATE),'MM');
+exit
