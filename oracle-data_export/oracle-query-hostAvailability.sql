67b18f02484d878a1a54f42f9136ed93f26e6e5d
Kev1venteur <kvin.gillet@gmail.com>
2021-04-14 12:48:43 +0200

    add sql request for cluster and default value of 100 if no blackout

--- oracle-data_export/oracle-query-hostAvailability.sql
+++ oracle-data_export/oracle-query-hostAvailability.sql
@@ -1,6 +1,6 @@
 -- This file must end in a new line.
 -- Total Percentage UP in the current month
-select round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 3)
+select NVL(round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 3), 100)
 from sysman.MGMT$AVAILABILITY_HISTORY
 where target_type = 'host'
 and availability_status = 'Blackout'
