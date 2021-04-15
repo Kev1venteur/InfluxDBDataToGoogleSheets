aa1fc05c79da2e9488357c129b5e64a738cbb1cb
Kev1venteur <kvin.gillet@gmail.com>
2021-04-15 13:25:41 +0200

    change number after comma, and remove blackout time in favor of target down time

--- oracle-data_export/oracle-query-hostAvailability.sql
+++ oracle-data_export/oracle-query-hostAvailability.sql
@@ -1,10 +1,10 @@
 -- This file must end in a new line.
 -- Total Percentage UP in the current month from the first day to your date
 -- if value of blackout is null return 100
-select NVL(round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 3), 100)
+select NVL(round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 2), 100)
 from sysman.MGMT${dollar}AVAILABILITY_HISTORY
 where target_type = 'host'
-and availability_status = 'Blackout'
+and availability_status = 'Target Down'
 and target_name like '%${envhostname}%'
 and TO_CHAR(start_timestamp,'MM') = TO_CHAR(add_months(trunc(sysdate,'mm'),-1),'MM')
 and TO_CHAR(end_timestamp,'MM') < TO_CHAR((sysdate),'MM');
