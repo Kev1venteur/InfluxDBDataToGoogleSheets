8dd052e7656b44cb57fa875b3bc2f78cd8a215b1
Kev1venteur <kvin.gillet@gmail.com>
2021-04-15 13:34:26 +0200

    set number of days from previous month for total of seconds (error before - current month sec counted)

--- oracle-data_export/oracle-query-clusterAvailability.sql
+++ oracle-data_export/oracle-query-clusterAvailability.sql
@@ -1,7 +1,7 @@
 -- This file must end in a new line.
 -- Total Percentage UP in the current month from the first day to your date
 -- if value of blackout is null return 100
-select NVL(round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(SYSDATE),'DD')*24*60*60), 2), 100)
+select NVL(round(100-(100*sum(round(60*60*24*(to_date(TO_CHAR(end_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') - to_date(TO_CHAR(start_timestamp,'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS')))))/(TO_CHAR(LAST_DAY(add_months(SYSDATE,-1)),'DD')*24*60*60), 2), 100)
 from sysman.MGMT${dollar}AVAILABILITY_HISTORY
 where target_type = 'cluster'
 and availability_status = 'Target Down'
